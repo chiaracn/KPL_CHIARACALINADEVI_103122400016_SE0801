30f4d92ba7cf7af4764620cfd1aa627e82a79975
chiaracn <chiaraacalina@gmail.com>
2026-04-13 22:26:04 +0700

    modul 07

---
+++ 07_Grammar_based_Input_Processing/TP/script.js
@@ -0,0 +1,13 @@
+function toNumberArray(number) {
+  return number
+    .split(",")
+    .map(item => item.trim())
+    .map(item => Number(item))
+    .filter(item => !isNaN(item));
+}
+
+// Testing
+console.log(toNumberArray("1, 2"));
+console.log(toNumberArray("1 , 2"));
+console.log(toNumberArray("11,55,33"));
+console.log(toNumberArray("0.2, -11, abc23"));
