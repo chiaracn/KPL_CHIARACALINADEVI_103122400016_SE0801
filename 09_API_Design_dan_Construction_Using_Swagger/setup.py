de51ed17d06d73973e86d7e8536089a015f12ce3
chiaracn <chiaraacalina@gmail.com>
2026-05-04 09:35:25 +0700

    Add files via upload

---
+++ 09_API_Design_dan_Construction_Using_Swagger/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_library",
+    version="0.1",
+    packages=find_packages(),
+    description="library sederhana untuk operasi matematika",
+    author="Chiaracalinadevi",
+    author_email="chiaracalinadevi@telkomuniversity.ac.id",
+)
