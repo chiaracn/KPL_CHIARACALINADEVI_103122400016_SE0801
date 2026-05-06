cbd34548484672d7f200ff5264634428cf1a6a3e
chiaracn <chiaraacalina@gmail.com>
2026-05-06 09:15:57 +0700

    TP 10

---
+++ 10_Library_Construction/TP/index.js
@@ -0,0 +1,6 @@
+import { hitungHuruf, hitungKata } from "./src/stringUtils.js";
+
+const teks = "Hallo, ini  tugas TP modul 10 nya ChiaraAa";
+
+console.log("Jumlah huruf:", hitungHuruf(teks));
+console.log("Jumlah kata:", hitungKata(teks));
