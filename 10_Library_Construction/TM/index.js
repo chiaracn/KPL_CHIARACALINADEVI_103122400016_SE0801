ee8f5edb34630efdfb3481a32ef78b6a826ae715
chiaracn <chiaraacalina@gmail.com>
2026-05-11 22:31:52 +0700

    TM10

---
+++ 10_Library_Construction/TM/index.js
@@ -0,0 +1,9 @@
+import pangkat from "./lib/pangkat.js";
+import bulat from "./lib/bulat.js";
+import kuadrat from "./lib/kuadrat.js";
+
+console.log(pangkat(2, 10));
+console.log(bulat(4.25));
+console.log(kuadrat(12));
+
+export { pangkat, bulat, kuadrat };
