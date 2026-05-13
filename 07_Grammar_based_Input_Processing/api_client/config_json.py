011f9c50b12ad78bae0dab8863792470dae2cf3c
chiaracn <chiaraacalina@gmail.com>
2026-05-13 11:06:00 +0700

    Delete 07_Grammar_based_Input_Processing/api_client directory

--- 07_Grammar_based_Input_Processing/api_client/config_json.py
+++
@@ -1,9 +0,0 @@
-import json
-
-# load configuration from JSON file atau deserilize JSON string
-def get_base_url():
-    with open("config.json", "r") as f:
-        config = json.load(f) 
-        base_url = config["base_url"]
-    return base_url
-
