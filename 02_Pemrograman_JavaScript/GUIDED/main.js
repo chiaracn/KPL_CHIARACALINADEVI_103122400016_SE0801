1ab8ed4571ab4a57df44ef31e2a91708fb11bfb5
chiaracn <chiaraacalina@gmail.com>
2026-05-27 15:37:28 +0700

    Add files via upload

---
+++ 02_Pemrograman_JavaScript/GUIDED/main.js
@@ -0,0 +1,10 @@
+let listMinuman = ["ice chocolate", "milktea", "orange jus"];
+console.log(listMinuman);
+
+listMinuman[0] = "thai tea";
+listMinuman[1] = "strawberry tea";
+
+console.log(listMinuman);
+
+listMinuman.unshift("hazelnut milk");
+console.log(listMinuman);
